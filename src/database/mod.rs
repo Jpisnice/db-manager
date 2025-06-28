ccf5b488b2e5a3985e3736414fbe7a0c43c5f80c
Jpisnice <www.janardhanpolle@gmail.com>
2025-06-28 13:51:02 +0530

    2nd commit

---
+++ src/database/mod.rs
@@ -0,0 +1,25 @@
+use serde::{Serialize, Deserialize};
+use std::path::PathBuf;
+use std::collections::HashMap;
+
+enum DbType {
+    POSTGRESS,
+    CHROMA,
+    REDIS,
+}
+#[derive(Serialize, Deserialize)]
+struct AppConfig {
+    passphrase_hash: String,
+    salt: Vec<u8>,
+    databases: HashMap<String, EncryptedDbConfig>,
+    version: u32, // for future migrations
+}
+
+#[derive(Serialize, Deserialize)]
+struct EncryptedDbConfig {
+    name: String,
+    db_type: DbType,
+    encrypted_credentials: Vec<u8>,
+    nonce: Vec<u8>,
+    created_at: chrono::DateTime<chrono::Utc>,
+}
